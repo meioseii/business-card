66bb512d6f9ba0865e91eb1c0ec70f0e2400ab07
Austin <moreno.austin12@gmail.com>
2023-01-01 22:09:07 +0800

    changed the about section

--- src/About.jsx
+++ src/About.jsx
@@ -5,7 +5,7 @@ export default function About() {
     return (
         <div className='about-container'>
             <h4 className='about-header'>About</h4>
-            <div className='about-info'>I am a college student taking a degree in Computer Science. I aspire to be a Software Engineer in the future. I enjoy programming because of the challenges it gives.</div>
+            <div className='about-info'>An enthusiastic developer that always seeks challenges to improve his skills. He is a team player, a good listener, a problem solver, and a persevering individual.</div>
         </div>
     )
 }
--- src/Info.jsx
+++ src/Info.jsx
@@ -5,7 +5,7 @@ export default function Info() {
     return (
         <div className='info'>
             <div className='name'>Austin Moreno</div>
-            <div className='profession'>College Student</div>
+            <div className='profession'>Frontend Developer</div>
             <div className='site'><small>https://github.com/meioseii</small></div>
         </div>
     )
